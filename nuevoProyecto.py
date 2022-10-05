0f218a75e8f4be2d4e59f034956aea035bb6f99a
Camilo Saenz <titanasura@gmail.com>
2022-10-04 22:08:08 -0500

    inicio de interfaz grafica

---
+++ nuevoProyecto.py
@@ -0,0 +1,8 @@
+from tkinter import *
+raiz=Tk()
+raiz.title('Casaroma')
+raiz.resizable(False,False)
+raiz.iconbitmap('pictures/iconoCasaroma.ico')
+raiz.geometry('400x400')
+raiz.config(bg='brown')
+raiz.mainloop()
